6743889c4b7e603ae2da0a49c48ee4c8c3c644ed
abuwahid9 <abuwahid9@gmail.com>
2024-11-30 00:37:18 +0600

    3rd

--- 2nd/char_print.c
+++ 2nd/char_print.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char name[] = {"Sheikh Tamim Mahmud"};
+    // char name = {"Abu Wahid"};
+    // printf("%c", name);
+    // char myText = {"Abu Wahid"};
+    // printf("%c", myText);
+    char name[] = {"Abu Wahid"};
     printf("%s", name);
     return 0;
 }
---
+++ 3rd/String1.c
@@ -0,0 +1,15 @@
+// 2. Find the Length of a String
+#include <stdio.h>
+#include <string.h> // For strlen
+
+int main()
+{
+    char str[] = "Programming in C";
+
+    // Using strlen function
+    int length = strlen(str);
+
+    printf("The length of the string is: %d\n", length);
+
+    return 0;
+}
